9388d142edb282fc3495d899bfb4d06c0eaff041
Esteban <esteban.salazar.p@gmail.com>
2019-07-23 10:30:12 -0400

    minify js css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,187 @@
-!function(e){"use strict";e(window).on("load",function(){e("#preloader").length&&e("#preloader").delay(100).fadeOut("slow",function(){e(this).remove()})}),e(window).scroll(function(){e(this).scrollTop()>100?e(".back-to-top").fadeIn("slow"):e(".back-to-top").fadeOut("slow")}),e(".back-to-top").click(function(){return e("html, body").animate({scrollTop:0},1500,"easeInOutExpo"),!1}),(new WOW).init(),e(window).scroll(function(){e(this).scrollTop()>100?e("#header").addClass("header-scrolled"):e("#header").removeClass("header-scrolled")}),e(window).scrollTop()>100&&e("#header").addClass("header-scrolled"),e(".main-nav a, .mobile-nav a, .scrollto").on("click",function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=e(this.hash);if(t.length){var a=0;return e("#header").length&&(a=e("#header").outerHeight(),e("#header").hasClass("header-scrolled")||(a-=20)),e("html, body").animate({scrollTop:t.offset().top-a},1500,"easeInOutExpo"),e(this).parents(".main-nav, .mobile-nav").length&&(e(".main-nav .active, .mobile-nav .active").removeClass("active"),e(this).closest("li").addClass("active")),e("body").hasClass("mobile-nav-active")&&(e("body").removeClass("mobile-nav-active"),e(".mobile-nav-toggle i").toggleClass("fa-times fa-bars"),e(".mobile-nav-overly").fadeOut()),!1}}});var t=e("section"),a=e(".main-nav, .mobile-nav"),o=e("#header").outerHeight();e(window).on("scroll",function(){var n=e(this).scrollTop();t.each(function(){var t=e(this).offset().top-o,r=t+e(this).outerHeight();n>=t&&n<=r&&(a.find("li").removeClass("active"),a.find('a[href="#'+e(this).attr("id")+'"]').parent("li").addClass("active"))})}),e('[data-toggle="counter-up"]').counterUp({delay:10,time:1e3}),e(window).on("load",function(){var t=e(".portfolio-container").isotope({itemSelector:".portfolio-item"});e("#portfolio-flters li").on("click",function(){e("#portfolio-flters li").removeClass("filter-active"),e(this).addClass("filter-active"),t.isotope({filter:e(this).data("filter")})})}),e(".testimonials-carousel").owlCarousel({autoplay:!0,dots:!0,loop:!0,items:1});e(".animated-text").each(function(){var t=e(this).text();for(var a in e(this).text(""),t){t[a];var o='<div class="{{class}}">{{character}}</div>'.replace("{{character}}",t[a]);e(this).append(o)}})}(jQuery);const typedTextSpan=document.querySelector(".typed-text"),cursorSpan=document.querySelector(".cursor"),textArray=["INSPIRACIÓN","CONOCIMIENTO","ACCIÓN"],typingDelay=60,erasingDelay=60,newTextDelay=750;let textArrayIndex=0,charIndex=0;function type(){charIndex<textArray[textArrayIndex].length?(cursorSpan.classList.contains("typing")||cursorSpan.classList.add("typing"),typedTextSpan.textContent+=textArray[textArrayIndex].charAt(charIndex),charIndex++,setTimeout(type,typingDelay)):(cursorSpan.classList.remove("typing"),setTimeout(erase,newTextDelay))}function erase(){charIndex>0?(cursorSpan.classList.contains("typing")||cursorSpan.classList.add("typing"),typedTextSpan.textContent=textArray[textArrayIndex].substring(0,charIndex-1),charIndex--,setTimeout(erase,erasingDelay)):(cursorSpan.classList.remove("typing"),++textArrayIndex>=textArray.length&&(textArrayIndex=0),setTimeout(type,typingDelay+1100))}document.addEventListener("DOMContentLoaded",function(){textArray.length&&setTimeout(type,newTextDelay+250)});
+(function ($) {
+  "use strict";
+
+  // Preloader (if the #preloader div exists)
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Initiate the wowjs animation library
+  new WOW().init();
+
+  // Header scroll class
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Smooth scroll for the navigation and links with .scrollto classes
+  $('.main-nav a, .mobile-nav a, .scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
+
+          if (! $('#header').hasClass('header-scrolled')) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.main-nav, .mobile-nav').length) {
+          $('.main-nav .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('.mobile-nav-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.main-nav, .mobile-nav');
+  var main_nav_height = $('#header').outerHeight();
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+  
+    nav_sections.each(function() {
+      var top = $(this).offset().top - main_nav_height,
+          bottom = top + $(this).outerHeight();
+  
+      if (cur_pos >= top && cur_pos <= bottom) {
+        main_nav.find('li').removeClass('active');
+        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
+      }
+    });
+  });
+
+  // jQuery counterUp (used in Whu Us section)
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Porfolio isotope and filter
+  $(window).on('load', function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+    $('#portfolio-flters li').on( 'click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+  
+      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    });
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+  
+  var $config = {
+    animatedText: '.animated-text',
+};
+var templateText = '<div class="{{class}}">{{character}}</div>';
+
+$($config.animatedText).each(function(){
+  var text = $(this).text();
+  $(this).text('');
+
+  for (var key in text){
+    var character = text[key];
+      var templatedCharacter = templateText.replace('{{character}}', text[key]);
+      $(this).append(templatedCharacter);
+  }
+})
+
+})(jQuery);
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["INSPIRACIÓN","CONOCIMIENTO","ACCIÓN"];
+const typingDelay = 60;
+const erasingDelay = 60;
+const newTextDelay = 750; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
 
 
 
